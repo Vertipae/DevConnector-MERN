1c4d1bdc1357d15345ad4f33d8fd675c247f45fc
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-01-25 21:22:59 +0200

    Added validation for registering and logging

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load user model
 const User = require("../../models/User");
@@ -67,6 +68,13 @@ router.post("/register", (req, res) => {
 // @desc    Login user / Returning JWT Token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body); // Pulling the data from register.js
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -75,7 +83,8 @@ router.post("/login", (req, res) => {
     .then(user => {
       // Check for user
       if (!user) {
-        return res.status(404).json({ email: "User not found" });
+        errors.email = "User not found";
+        return res.status(404).json(errors);
       }
 
       // Check password when user is found
@@ -98,7 +107,8 @@ router.post("/login", (req, res) => {
           );
         } else {
           // If pw doesn't match status 400
-          return res.status(400).json({ password: "Password incorrect" });
+          errors.password = "Password is incorrect";
+          return res.status(400).json(errors);
         }
       });
     });
--- validation/register.js
+++ validation/register.js
@@ -4,9 +4,39 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  // If the name isn't empty then it is going to be data.name but if it is empty then it's going to be empty string
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     // If its not between 2 and 30 it gives error
     errors.name = "Name must be between 2 and 30 characters";
+  }
+  // Goes to here as a empty string
+  // Gives error if the name is empty
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
+  }
+  // Validating that Password and Password 2 matches (if its not equal)
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
   }
 
   return {
