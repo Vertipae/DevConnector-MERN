1980d490ceee77ec92c9045df4cf4450c4ea12cd
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-01-26 17:58:26 +0200

    Added create & update profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,3 +1,4 @@
+// Awesome profile route
 const express = require("express");
 const router = express.Router();
 // For database
@@ -36,6 +37,67 @@ router.get(
         res.json(profile);
       }) // If something still goes wrong with the findOne then it gives generated error
       .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   POST api/profile
+// @desc    Create or Edit user profile
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id; // Includes avatar, name and email
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Skills - split into array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(","); // Awesome splitting
+    }
+
+    // Social (is in its own object in Profile.js and has object of fields)
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }) // Finds logged in user
+      .then(profile => {
+        // If profile exists
+        if (profile) {
+          // Update
+          Profile.findOneAndUpdate(
+            { user: req.user.id },
+            { $set: profileFields },
+            { new: true }
+          ) // Determines what user to update
+            .then(profile => res.json(profile));
+        } else {
+          // Create
+          // Check if handle exists
+          Profile.findOne({ handle: profileFields.handle }).then(profile => {
+            // If profile exists with that handle then it sends an error
+            if (profile) {
+              errors.handle = "That handle already exists";
+              res.status(400).json(errors);
+            }
+            // If there isn't a profile with that handle then it saves a profile
+            new Profile(profileFields)
+              .save()
+              .then(profile => res.json(profile));
+          });
+        }
+      });
   }
 );
 
