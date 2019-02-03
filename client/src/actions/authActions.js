2efdd84aa5bd01210ed48f19d976d46f2ff6cc87
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-02-03 19:30:50 +0200

    Set token to localStorage

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+
 import { GET_ERRORS } from "./types";
+
 // Register user
 // Thunk allows to write action creators that return a function instead of an action (dispatch)
 export const registerUser = (userData, history) => dispatch => {
@@ -9,6 +12,26 @@ export const registerUser = (userData, history) => dispatch => {
     .then(res => history.push("/login"))
     // .catch(err => console.log(err))
     //   .catch(err => console.log(err.response.data));
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login - Get user Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
