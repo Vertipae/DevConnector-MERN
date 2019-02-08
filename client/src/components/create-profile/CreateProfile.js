e91187e7becab345bea3e42fbf9c6438e349e3c7
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-02-08 20:45:27 +0200

    Added form field components

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import InputGroup from "../common/InputGroup";
+import SelectListGroup from "../common/SelectListGroup";
 import { Redirect } from "react-router-dom";
 
 class CreateProfile extends Component {
