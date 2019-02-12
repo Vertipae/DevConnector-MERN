2ed5ee24c800b6f5ec740c5d1b7d4c4868fd2d4c
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-02-12 13:46:32 +0200

    Added dashboard education display & delete

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { deleteEducation } from "../../actions/profileActions";
+
+class Education extends Component {
+  onDeleteClick = id => {
+    this.props.deleteEducation(id);
+  };
+
+  render() {
+    const education = this.props.education.map(edu => (
+      <tr key={edu._id}>
+        <td>{edu.school}</td>
+        <td>{edu.degree}</td>
+        <td>
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          {/* If the current date is null it will display Now else shows edu.to date */}
+          {edu.to === null ? (
+            " Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{edu.to}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            onClick={this.onDeleteClick.bind(this, edu._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
+
+    // if (!this.props.auth.isAuthenticated) {
+    //   return <Redirect to="/login" />;
+    // }
+
+    return (
+      <div>
+        <h4 className="mb-4">Education Credentials</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>School</th>
+              <th>Degree</th>
+              <th>Years</th>
+              <th />
+            </tr>
+            {education}
+          </thead>
+        </table>
+      </div>
+    );
+  }
+}
+
+Education.propTypes = {
+  deleteEducation: PropTypes.func.isRequired
+};
+
+// const mapStateToProps = state => ({
+//   auth: state.auth
+// });
+
+export default connect(
+  null,
+  { deleteEducation }
+)(Education);
