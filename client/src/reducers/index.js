e919dcaac46b3c7c707939b0e7acaa5de40c1dda
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-02-14 11:44:49 +0200

    Added post state & addPost action

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
 
 // Reducers put in object (example: auth is usable when calling an action)
 const rootReducer = combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
 
 export default rootReducer;
